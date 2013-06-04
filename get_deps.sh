f473a56068e0504f825042345acfa5d6d5bce6ba
Nelson Silva <nelson.silva@inevo.pt>
2013-06-04 16:10:49 +0100

    Initial import

---
+++ get_deps.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+mkdir lib
+wget -O lib/java-property-utils-1.9.jar http://repo1.maven.org/maven2/com/thetransactioncompany/java-property-utils/1.9/java-property-utils-1.9.jar
+wget -O lib/cors-filter-1.7.jar http://repo1.maven.org/maven2/com/thetransactioncompany/cors-filter/1.7/cors-filter-1.7.jar
